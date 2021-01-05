7e765b6d218b89a86349ff7da80acc59f1349a99
Vicky Vasilopoulou <vickyvasilopoulou@Vickys-MacBook.local>
2021-01-05 18:48:12 +0000

    feat:add theme on form.js file

--- src/components/form.js
+++ src/components/form.js
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
 import { Input } from '@material-ui/core';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
     input: {
         width: '250px',
         marginBottom: '40px',
